4186cf680a8a520eb712bf5c94ec94d4207724c6
Adams-Amazon <aejeh98@gmail.com>
2026-04-21 13:42:41 +0100

    Make the cart quantity interactive

--- script/amazon.js
+++ script/amazon.js
@@ -65,21 +65,28 @@
 
          let matchingItem;
 
-         cart.forEach(() =>{
-            if (productName === Item.productName){
+         cart.forEach((item) =>{
+            if (productId === item.productId){
                 matchingItem = item;
             } 
          });
 
          if (matchingItem){
-          matchingItem.quantity +=1;
+          matchingItem.quantity += 1;
          }else {
             cart.push({
             productId: productId,
             quantity:1
-         })
+         });
          }
-      
-      console.log(cart);
+
+         let cartQuantity = 0;
+
+          cart.forEach((item) => {
+            cartQuantity += item.quantity;
+          });
+
+          document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     });
   }); 
