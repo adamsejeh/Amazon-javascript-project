8a2b4de1122d522732a4dbdd3ed90a25284729c8
Adams-Amazon <aejeh98@gmail.com>
2026-04-24 11:49:54 +0100

    Delete product from cart when clicking delete

--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -40,7 +40,7 @@ cart.forEach((cartItem) => {
               <span class="update-quantity-link link-primary">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary">
+              <span class="delete-quantity-link link-primary js-delete-link" data-product-Id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -53,7 +53,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
               <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-${matchingProduct.Id}">
+                name="delivery-option-${matchingProduct.id}">
               <div>
                 <div class="delivery-option-date">
                   Tuesday, June 21
@@ -66,7 +66,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
               <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
+                name="delivery-option-${matchingProduct.Id}">
               <div>
                 <div class="delivery-option-date">
                   Wednesday, June 15
@@ -79,7 +79,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
               <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
+                name="delivery-option-${matchingProduct.Id}">
               <div>
                 <div class="delivery-option-date">
                   Monday, June 13
@@ -97,3 +97,14 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+
+      const productId = link.dataset.productId;
+      // console.log(productId);
+      removeFromCart(productId);
+      console.log(cart); 
+    });
+  });
